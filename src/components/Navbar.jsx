330dbc579f41ff5d1661340560c8bc74ec37c606
Pranav Salunkhe <pvsalunkhe2003@gmail.com>
2023-10-05 17:32:53 +0530

    added starter code

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='flex flex-row w-full justify-between py-5 px-7'>
+    <div className='basis-1/3'>LOGO</div>
+    <div className='basis-2/3'>
+        <ul className='flex justify-evenly'>
+            <Link href='test/me'>l1</Link>
+            <Link href="">l1</Link>
+            <Link href="">l1</Link>
+            <Link href="">l1</Link>
+        </ul>
+    </div>
+    </div>
+  )
+}
---
+++ src/pages/test/me.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function me() {
+  return (
+    <div className='h-screen'>me</div>
+  )
+}
+
+export default me
